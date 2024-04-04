56f91d71ac4b04d01f9dfdcc71ce152cb9e1233d
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-04 11:30:43 -0300

    feat: Adicionando funcionalidades do formulário

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,36 @@
+import { useState, FormEvent } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function Home() {
+
+    const [input, setInput] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e : FormEvent) {
+        e.preventDefault();
+
+        if( input === '') {
+            return;
+        } else {
+            navigate(`/detail/${input}`)
+        }
+
+    }
+    function handleGetMore() {
+        alert('clicou no carregar mais')
+    }
+
     return(
         <main className={styles.container}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <input 
                 type='text'
                 placeholder='Digite o nome da moeda... EX: bitcoin'
+                value={input}
+                onChange={ (e) => setInput(e.target.value) }
                 />
                 <button type='submit'>
                     <BsSearch size={30} color='#fff'/>
@@ -52,6 +74,10 @@ export function Home() {
 
                 </tbody>
             </table>
+
+            <button className={styles.buttonMore} onClick={handleGetMore}>
+                Carregar mais
+            </button>
 
         </main>
     )
