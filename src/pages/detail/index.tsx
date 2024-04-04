74565797d8c76cd7663a2de2dcc516366b4de815
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-04 16:37:20 -0300

    feat: Páginas dos detalhes da moeda

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,9 +1,68 @@
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { CoinProps } from "../home";
 
+interface ResponseData {
+    data: CoinProps
+}
+
+interface ErrorData {
+    error: string
+}
+
+type DataProps = ResponseData | ErrorData
 
 export function Detail() {
+
+    const { cripto } = useParams();
+    const navigate = useNavigate();
+
+    const [moeda, setMoeda] = useState<CoinProps>();
+
+    useEffect( () => {
+        async function getCoin() {
+            try {
+                fetch(`https://api.coincap.io/v2/assets/${cripto}`)
+                .then(response => response.json())
+                .then((data : DataProps) => {
+                    if("error" in data) {
+                        navigate('/')
+                        return;
+                    }
+                    
+                    const price = Intl.NumberFormat("en-US", {
+                        style: 'currency',
+                        currency: 'USD'
+                    })
+        
+                    const priceCompact = Intl.NumberFormat("en-US", {
+                        style: 'currency',
+                        currency: 'USD',
+                        notation: 'compact'
+                    })
+
+                    const resultData = {
+                        ...data.data,
+                        formatedPrice: price.format(Number(data.data.priceUsd)),
+                        formatedMarket: priceCompact.format(Number(data.data.marketCapUsd)),
+                        formatedVolume: priceCompact.format(Number(data.data.volumeUsd24Hr))
+                    }
+                    
+                    setMoeda(resultData)
+
+                })
+            } catch(err) {
+                console.log(err);
+                navigate('/')
+            }
+        }
+
+        getCoin();
+    }, [cripto])
+    
     return(
         <div>
-            <h1>Pagina detalhe da moeda</h1>
+            <h1>Pagina detalhe da moeda {cripto}</h1>
         </div>
     )
 }
